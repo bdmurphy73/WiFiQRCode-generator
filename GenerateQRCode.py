26b67612bd87d184e676353ee0e2efaa67c01ed3
bdmurphy73 <161863146+bdmurphy73@users.noreply.github.com>
2024-03-04 08:12:02 -0800

    Changed to have generic example of QR Code.

--- GenerateQRCode.py
+++ GenerateQRCode.py
@@ -13,9 +13,9 @@ import wifi_qrcode_generator.generator
 from PIL import Image, ImageDraw, ImageFont
 
 #Set your variables. They are all in one spot to make it easier
-myssid = 'BDMurphyGuestWiFi'
-mypasswd = 'bdM#WiFi$Password%'
-imagefile = 'bdmguestwifiQrCode.png'
+myssid = 'GuestWi-Fi'
+mypasswd = 'LongPassword'
+imagefile = 'ExampleWiFiQRCode.png'
 
 #Generate the QRCode of the WiFi login
 qr_code = wifi_qrcode_generator.generator.wifi_qrcode(
