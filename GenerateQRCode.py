1103243898e0fc6de0d3f61fba000249139299e5
B.D. Murphy <161863146+bdmurphy73@users.noreply.github.com>
2024-03-24 07:46:30 -0700

    Update GenerateQRCode.py
    Added update to comments in the file

--- GenerateQRCode.py
+++ GenerateQRCode.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-
+# Published by B.D. Murphy @ 2024
+# This script can be used to create a QR code that users can scan to have their device connect to a WiFi network.
+# It requires user intput below for the network SSID, password, and output file name.
+# The generated QR code will also have text below so users can type in the information if their device won't scan.
 # The module to create the QR code can be found: https://pypi.org/project/wifi-qrcode-generator/
 
 #install with: pip install wifi_qrcode_generator.generator
@@ -8,15 +11,17 @@ import wifi_qrcode_generator.generator
 # You need pillow module for image processing
 # https://pypi.org/project/pillow/
 # Install with: pip install pillow
-
 #import image processing modules
 from PIL import Image, ImageDraw, ImageFont
 
+
+####### USER INPUT ###########
 #Set your variables. They are all in one spot to make it easier
 myssid = 'GuestWi-Fi'
 mypasswd = 'LongPassword'
 imagefile = 'ExampleWiFiQRCode.png'
 
+######## Processing start
 #Generate the QRCode of the WiFi login
 qr_code = wifi_qrcode_generator.generator.wifi_qrcode(
     ssid=myssid, hidden=False, authentication_type='WPA', password=mypasswd
@@ -25,7 +30,7 @@ qr_code = wifi_qrcode_generator.generator.wifi_qrcode(
 #qr_code.print_ascii()
 qr_code.make_image().save("Tmpimg.png")
 
-#Now we need to adjust the QRCode image
+#Now we need to adjust the QRCode image to add the text below
 
 # Open the image 
 img = Image.open("Tmpimg.png")
@@ -43,11 +48,11 @@ new_img.paste(img, (0, 0))
 # Create a drawing object
 draw = ImageDraw.Draw(new_img)
 
-# Load a custom font (replace 'FreeMono.ttf' with the path to your font file)
+# Load a custom font (replace 'cour.ttf' with the path to your font file)
 # Using basic courier font for the text under the QRCode
 myFont = ImageFont.truetype('\windows\fonts\cour.ttf', 35)
 
-# Add text with the defined font style
+# Add text to the image with the defined font style
 # Put the SSID at 90 pixels from the bottom of the image. Color is black.
 # Put the password at 45 pixels from the bottom
 # A guest can read and type in the text if the scan doesn't work.
